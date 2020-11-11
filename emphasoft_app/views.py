a31526708b1f372d233961eaf902c4b253107a68
Edgar Zhivaev <edyapups@gmail.com>
2020-11-11 14:38:22 +0300

    Init views.

--- emphasoft_app/views.py
+++ emphasoft_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+def login(request):
+    pass
+
+
+def index(request):
+    pass
 
 # Create your views here.
