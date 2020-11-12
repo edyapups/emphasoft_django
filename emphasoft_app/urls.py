da7c173231932e575900c7c5ef3eab57a87b34cf
Edgar Zhivaev <edyapups@gmail.com>
2020-11-12 17:55:54 +0300

    Login fixes.

--- emphasoft_app/urls.py
+++ emphasoft_app/urls.py
@@ -5,8 +5,9 @@ from . import views
 app_name = 'emphasoft_app'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('me/', views.me, name='me'),
     path('user/<int:user_id>/', views.user_page, name='user'),
-    path('edit_profile', views.edit_user, name='edit_user'),
+    path('edit_profile/', views.edit_user, name='edit_user'),
     path('login/', views.login, name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
