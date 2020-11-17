47fce482a7b5cfeff203945d6bf959789e01f3c3
Edgar Zhivaev <edyapups@gmail.com>
2020-11-17 13:54:25 +0300

    Wrote documentation for views.

--- emphasoft_app/views.py
+++ emphasoft_app/views.py
@@ -7,23 +7,44 @@ from .models import User
 
 
 def login(request):
+    """
+    Renders an authentication page.
+
+    :param request: Request object.
+    """
     return render(request, 'emphasoft_app/login.html')
 
 
 @login_required()
 def me(request):
+    """
+    Redirects to the authenticated user page.
+
+    :param request: Request object.
+    """
     return redirect('emphasoft_app:user', user_id=request.user.id)
 
 
 @login_required()
 def index(request):
+    """
+    Renders the main page of the site with a list of all registered users.
+
+    :param request: Request object.
+    """
     return render(request, 'emphasoft_app/index.html', context={
         'users': User.objects.all().order_by('-last_login'),
     })
 
 
 @login_required()
-def user_page(request, user_id):
+def user_page(request, user_id: int):
+    """
+    Renders a user page.
+
+    :param request: Request object.
+    :param user_id: int: The ID of the user whose page you want to display.
+    """
     try:
         user = User.objects.get(pk=user_id)
         return render(request, 'emphasoft_app/user.html', context={
@@ -36,6 +57,12 @@ def user_page(request, user_id):
 @login_required()
 @transaction.atomic()
 def edit_user(request):
+    """
+    Modifies the authenticated user data if the request method is POST,
+    or renders the authenticated user data change form otherwise.
+
+    :param request:
+    """
     if request.method == "POST":
         user_form = forms.UserForm(request.POST, instance=request.user)
         profile_form = forms.ProfileForm(request.POST, instance=request.user.profile, files=request.FILES)
